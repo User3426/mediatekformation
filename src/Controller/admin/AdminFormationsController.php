aae97115ced08396fc89a8dffcaacb2c719085d4
User3426 <tristanrage.adresse@gmail.com>
2025-11-05 16:15:46 +0100

    Mission 2.1.1 : Supprimer des formations

---
+++ src/Controller/admin/AdminFormationsController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller\admin;
+
+use App\Entity\Formation;
+use App\Repository\CategorieRepository;
+use App\Repository\FormationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AdminFormationsController
+ *
+ * @author Tristan
+ */
+class AdminFormationsController extends AbstractController{
+    
+    
+    private const PAGEADMINFORMATIONS = "admin/admin.formations.html.twig";
+    
+    /**
+     * 
+     * @var FormationRepository
+     */
+    private $formationRepository;
+    
+    /**
+     * 
+     * @var CategorieRepository
+     */
+    private $categorieRepository;
+    
+    function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
+        $this->formationRepository = $formationRepository;
+        $this->categorieRepository= $categorieRepository;
+    }
+    
+    #[Route('/admin', name: 'admin.formations')]
+    public function index(): Response{
+        $formations = $this->formationRepository->findAll();
+        $categories = $this->categorieRepository->findAll();
+        return $this->render(self::PAGEADMINFORMATIONS, [
+            'formations' => $formations,
+            'categories' => $categories
+        ]);
+    }
+    
+    #[Route('admin/formations/tri/{champ}/{ordre}/{table}', name: 'admin.formations.sort')]
+    public function sort($champ, $ordre, $table=""): Response{
+        $formations = $this->formationRepository->findAllOrderBy($champ, $ordre, $table);
+        $categories = $this->categorieRepository->findAll();
+        return $this->render(self::PAGEADMINFORMATIONS, [
+            'formations' => $formations,
+            'categories' => $categories
+        ]);
+    }     
+
+    #[Route('admin/formations/recherche/{champ}/{table}', name: 'admin.formations.findallcontain')]
+    public function findAllContain($champ, Request $request, $table=""): Response{
+        $valeur = $request->get("recherche");
+        $formations = $this->formationRepository->findByContainValue($champ, $valeur, $table);
+        $categories = $this->categorieRepository->findAll();
+        return $this->render(self::PAGEADMINFORMATIONS, [
+            'formations' => $formations,
+            'categories' => $categories,
+            'valeur' => $valeur,
+            'table' => $table
+        ]);
+    }
+    
+    #[Route('/admin/formation/suppr/{id}', name: 'admin.formation.suppr')]
+    public function suppr(int $id): Response{
+        $formation = $this->formationRepository->find($id);
+        $this->formationRepository->remove($formation);
+        return $this->redirectToRoute('admin.formations');
+    }
+}
